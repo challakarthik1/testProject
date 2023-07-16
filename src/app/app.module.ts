24bbd82fab5ae4dc2ffb2ab81e28c8eaffe9df8c
Challa Karthik <karthik.netprofessional@gmail.com>
2023-07-16 23:57:50 +0530

    add case component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddCaseComponent } from './add-case/add-case.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCaseComponent
   ],
   imports: [
     BrowserModule,
